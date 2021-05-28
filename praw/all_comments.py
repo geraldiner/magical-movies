7bd0492780a91bf4efcc180827a4f3854908b9aa
geraldiner <hello@geraldiner.com>
2021-05-27 23:32:47 -1000

    reorganize

--- praw/all_comments.py
+++ praw/all_comments.py
@@ -2,6 +2,10 @@ import os
 import glob
 import json
 
+
+sum = 0
 for f in glob.glob("*.json"):
     with open(f) as inFile:
-        print(len(json.load(inFile)))
+        sum += len(json.load(inFile))
+
+print(sum)
