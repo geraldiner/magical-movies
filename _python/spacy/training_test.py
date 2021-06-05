b34c265a77a7a3eb5dd076d98b1ae91515c1886d
geraldiner <hello@geraldiner.com>
2021-06-04 22:58:27 -1000

    generate movies_200.json to use

--- _python/spacy/training_test.py
+++ _python/spacy/training_test.py
@@ -1,11 +1,25 @@
 import spacy
 import json
 
-test_sample = open("top_200_comments_.json", "r", encoding="utf-8")
+# test_sample = open("top_200_comments_.json", "r", encoding="utf-8")
+# data = json.load(test_sample)
+
+test_sample = open("../praw/praw_comments_part01.json", "r", encoding="utf-8")
 data = json.load(test_sample)
+
+sorted_data = sorted(data, key=lambda i: i["score"], reverse=True)
 
 nlp1 = spacy.load("./output/model-last")  # load the best model
 
+# Testing Code
+# for i in range(0, 200):
+#     doc = nlp1(data[i]["body"])
+#     for ent in doc.ents:
+#         text = data[i]["body"]
+#         print(text)
+#         print("\tMovie found: " + ent.text)
+
+# Printing Code
 resultFile = open("movies_200.json", "w", encoding="utf-8")
 movies = []
 
@@ -21,5 +35,5 @@ for i in range(0, 200):
         movies.append(movie)
 
 
-resultFile.write(json.dumps(movies))
+resultFile.write(json.dumps(sorted(movies, key=lambda i: i["score"], reverse=True)))
 resultFile.close()
