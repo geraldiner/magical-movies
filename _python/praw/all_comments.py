fd1a9c49c2df4c0b3cc48802b22a1491b12fa40d
geraldiner <hello@geraldiner.com>
2021-06-04 21:49:49 -1000

    only gonna work with 200 comments :)

--- _python/praw/all_comments.py
+++ _python/praw/all_comments.py
@@ -1,10 +1,28 @@
 import json
 import glob
+import merge
+import comments_sorted
 
-result = []
-for f in glob.glob("*.json"):
-    with open(f, "r") as infile:
-        result += json.load(infile)
+# result = []
+# for f in glob.glob("*.json"):
+#     with open(f, "r") as infile:
+#         result += json.load(infile)
 
-with open("merge.json", "w") as outfile:
-    json.dump(result, outfile)
+# with open("merge.json", "w") as outfile:
+#     json.dump(result, outfile)
+
+# all_comments = merge.all_comments
+# with open("comments_sorted.py", "w", encoding="utf-8") as outputFile:
+#     outputFile.write(json.dumps(sorted(all_comments, key=lambda i: i["score"], reverse=True)))
+
+# outputFile.close()
+
+comments_sorted = comments_sorted.comments_sorted
+top_100 = []
+for i in range(0,200):
+    top_100.append(comments_sorted[i])
+
+with open("top_200_comments.json", "w", encoding="utf-8") as outputFile:
+    outputFile.write(json.dumps(top_100))
+
+outputFile.close()
