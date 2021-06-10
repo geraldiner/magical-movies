f320f8bc514562bf1095be771c8f6ccb8cb60124
geraldiner <hello@geraldiner.com>
2021-06-09 21:42:03 -1000

    test layout with tailwind

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Movie from './Movie'
+import Movies from './Movies'
 
 const Home = () => {
   return (
     <div className="min-h-screen w-9/12 my-12 mx-auto bg-gray-200 sm:px-6 lg:px-8">
-      <Movie />
+      <Movies />
     </div>
   )
 }
---
+++ client/src/components/Movies.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Movie from './Movie'
+
+const Movies = () => {
+  return (
+    <div className="py-10">
+      <h1 className="text-4xl font-serif text-center">
+        <span className="text-6xl font-passion-one">"</span>What movie is simply magical to you, no matter how popular or unpopular it may be?
+        <div className="text-2xl">-u/Feelingofsunday</div>
+      </h1>
+      <div className="my-5 mx-3">
+        <Movie />
+        <Movie />
+        <Movie />
+      </div>
+    </div>
+  )
+}
+
+export default Movies
