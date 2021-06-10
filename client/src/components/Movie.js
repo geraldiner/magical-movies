7dfb89bac2f7ed73b79fb64fae31298b070e67e5
geraldiner <hello@geraldiner.com>
2021-06-09 21:25:43 -1000

    finally fixed the fonts

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,7 +4,7 @@ const Movie = () => {
   return (
     <div>
       <h1 className="text-4xl font-serif text-center">
-        <span className="text-6xl">"</span>What movie is simply magical to you, no matter how popular or unpopular it may be?
+        <span className="text-6xl font-passion-one">"</span>What movie is simply magical to you, no matter how popular or unpopular it may be?
         <div className="text-2xl">-u/Feelingofsunday</div>
       </h1>
     </div>
